c8a10da1534c818a0fb0d1889adcd268662bd6bb
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-05-08 15:58:00 +0200

    product datas retrieval from API

--- front/JS/script.js
+++ front/JS/script.js
@@ -1,1 +1,6 @@
 /* JS page linked to index.html */
+
+/* Récupération des produits depuis l'API : */
+const product = fetch("http://localhost:3000/api/products")
+    .then(product => product.json())
+
