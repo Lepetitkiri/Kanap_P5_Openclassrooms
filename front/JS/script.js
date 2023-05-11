7987c00eec9e4340797387306a8e257a677def75
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-05-11 14:43:52 +0200

    eslint file added

--- front/JS/script.js
+++ front/JS/script.js
@@ -1,32 +1,18 @@
 /* JS page linked to index.html */
 
 /* Récupération des produits depuis l'API : */
-const product = fetch("http://localhost:3000/api/products")
+const product = fetch('http://localhost:3000/api/products')
     .then(product => product.json())
-    
 
     /* Création des produits */
 for ( i = 0 ; i < 100; i++ ) {
-
     /* insertion de l'élèment <a> : */ 
     let productA = document.createElement("a");
     document.querySelector(".items").appendChild(productA);
-    productA.href = `./product.html?id=${i}`;
+    productA.href = `./product.html?id=${i}`
 
     /* insertion de l'élèment <article> dans le <a> : */
-    let productArticle = document.createElement("article");
+    let productArticle = document.createElement('article');
     productA.appendChild(productArticle);
-    productArticle.innerText = ``;
-
+    productArticle.innerText = ``
 }
-
-
-    
-
-
-
-
-
-
-
-
