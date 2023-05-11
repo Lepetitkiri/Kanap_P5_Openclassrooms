45bc206b5bcb585fd36bb874b33f8084c0d93cfc
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-05-11 15:00:31 +0200

    creation of <img>, <h3> and <p> elements on index.html

--- front/JS/script.js
+++ front/JS/script.js
@@ -3,9 +3,12 @@
 /* Récupération des produits depuis l'API : */
 const product = fetch('http://localhost:3000/api/products')
     .then(product => product.json())
+    .then(function(product) {productCreation(product)});
 
     /* Création des produits */
-for ( i = 0 ; i < 100; i++ ) {
+function productCreation(product) {
+    for ( i = 0 ; i < product.length; i++ ) {
+
     /* insertion de l'élèment <a> : */ 
     let productA = document.createElement("a");
     document.querySelector(".items").appendChild(productA);
@@ -15,4 +18,29 @@ for ( i = 0 ; i < 100; i++ ) {
     let productArticle = document.createElement('article');
     productA.appendChild(productArticle);
     productArticle.innerText = ``
+
+    /* insertion de l'élèment <img> dans l'<article> : */
+    let productImage = document.createElement("img");
+    productArticle.appendChild(productImage);
+    productImage.src = `src=".../back/images/${product[i].imageUrl}`;
+    
+    /* insertion de l'élèment <h3> dans <a> : */
+    let productName = document.createElement("h3");
+    productArticle.appendChild(productName);
+    productName.innerText = product[i].name;
+    
+    /* insertion de l'élèment <p> dans l'<a> : */
+    let productDescription = document.createElement("p");
+    productArticle.appendChild(productDescription);
+    productDescription.innerText = product[i].description;
+    }
 }
+
+    
+
+
+
+
+
+
+
