f5884488c3f5a1b0579be52a1fa71f305dfe2386
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-05-15 15:52:45 +0200

    ajout d'un message d'erreur et redirection vers index.html si la récupération de l'id de product.html ne se fait pas correctement

--- front/JS/product.js
+++ front/JS/product.js
@@ -5,3 +5,14 @@ const url = window.location.href;
 /* Extraction de l'ID depuis le href de la page */
 const urlIdsSplited = url.split('=');
 const urlId = urlIdsSplited[1];
+
+
+/* NON DEMANDé : Redirection vers index.html si la récupération du href n'est pas fait correctement */
+if (url.includes(`http`)) {
+    console.log(`l'URL de la page actuelle est : ${url}. L'id du produit sélectionné est : ${urlId}`);
+} else {
+   window.alert(`Vous allez être redirigé vers la page d'accueil. Veuillez nous excuser pour la gène occasionnée.`);
+   window.location.replace(`./index.html`)
+   const urlErrorDate = Date.now();
+   console.log(`Une erreur s'est produite lors de la redirection de l'utilisateur. Date de l'évenement : ${urlErrorDate}`);
+}
