f9346a8f8e6bdad643d847e0ae87acae4c872da5
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2023-06-22 12:28:01 +0200

    fonction d'affichage du numero de commande sur confirmation.js

--- front/JS/confirmation.js
+++ front/JS/confirmation.js
@@ -1,1 +1,11 @@
 /* JS page linked to confirmation.html */
+
+/* Récupération du href de la page */
+const url = window.location.href;
+/* Extraction de l'ID depuis le href de la page */
+const urlIdsSplited = url.split('=');
+const urlId = urlIdsSplited[1];
+
+/* Affichage sur la page */
+let spanOrderId = document.querySelector('span');
+spanOrderId.innerText = urlId;
